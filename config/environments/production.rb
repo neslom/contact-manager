5713195f72bb1c9c9cc7fc4c2564c57143ee15a4
Markus Olsen <molsen13@gmail.com>
2015-02-27 13:10:39 -0700

    Took out the config.serve_static_assets due to deprecation. Config.serve_static_files is the acceptable syntax, which was provided by Rails when starting up the app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,7 +35,12 @@ Rails.application.configure do
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
 
-  config.serve_static_assets = true
+  #config.serve_static_assets = true
+  # this syntax is deprecated; serve_static_files should be handling this
+
+
+
+
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Specifies the header that your server uses for sending files.
