652629b748815c42d4fc5330bcae7f4020a418ef
Markus Olsen <molsen13@gmail.com>
2015-02-26 21:55:29 -0700

    Validate first_name for Person model

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Person, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe ".create" do
+
+    context "without first name" do
+
+      it "is invalid" do
+        person = Person.create(first_name: nil)
+
+        expect(person).not_to be_valid
+      end
+
+    end
+
+  end
+
 end
