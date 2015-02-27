85563d3be0ae42b726c56dd103c441f18bd252f1
Markus Olsen <molsen13@gmail.com>
2015-02-27 13:04:05 -0700

    Confirm person is indeed valid before testing its attributes

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -2,14 +2,22 @@ require 'rails_helper'
 
 RSpec.describe Person, type: :model do
   describe ".create" do
+    let(:person) do
+      Person.new(first_name: 'Alice', last_name: 'Smith')
+    end
+
+    it "is valid" do
+      expect(person).to be_valid
+    end
+
     it "is invalid without a first name" do
-      person = Person.create(last_name: "Smith")
+      person.first_name = nil
 
       expect(person).to be_invalid
     end
 
     it "is invalid without a last name" do
-      person = Person.create(first_name: "Bob")
+      person.last_name = nil
 
       expect(person).not_to be_valid
     end
