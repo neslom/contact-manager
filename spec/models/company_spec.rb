9adb5b5f8ce64acb54ad3b29e6c1eb598aeb45f1
Markus Olsen <molsen13@gmail.com>
2015-02-28 16:26:49 -0700

    Company is valid

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Company, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:company) { Company.new(name: "Apple") }
+
+  it "is valid" do
+    expect(company).to be_valid
+  end
+
+  it "is invalid without a name" do
+    company.name = nil
+
+    expect(company).to be_invalid
+  end
+
+  it "has an array of phone numbers" do
+    expect(company.phone_numbers).to eq([])
+  end
 end
