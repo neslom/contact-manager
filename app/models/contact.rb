073e21def59afe452ef6b4a9015e73dcaf903ea1
Markus Olsen <molsen13@gmail.com>
2015-03-01 13:43:11 -0700

    Refactor phone_number view to accept passed in local variables from Company and Person

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,9 +2,8 @@ module Contact
   extend ActiveSupport::Concern
 
   included do
-  has_many :phone_numbers, as: :contact
-  has_many :email_addresses, as: :contact
-
+    has_many :phone_numbers, as: :contact
+    has_many :email_addresses, as: :contact
   end
 
   module ClassMethods
